400c684516d9c7044d0fa18fb0fd77a5214c254a
EliseoRoman <romaneliseo@hotmail.com>
2017-10-17 22:56:22 -0600

    asdf

--- src/javaadvancedoctubre/Lunes/Tareas/LoadImageApp.java
+++ src/javaadvancedoctubre/Lunes/Tareas/LoadImageApp.java
@@ -1,43 +1,12 @@
 package javaadvancedoctubre.Lunes.Tareas;
 
-/**
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
-
-
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
+import java.applet.AudioClip;
 
 public class LoadImageApp extends Component {
           
@@ -50,6 +19,9 @@ public class LoadImageApp extends Component {
     public LoadImageApp(String imagen) {
        try {
            img = ImageIO.read(new File(imagen));
+           AudioClip sonido;
+           sonido = java.applet.Applet.newAudioClip(getClass().getResource("/javaadvancedoctubre/Lunes/Tareas/audio1.wav"));
+           sonido.play();
        } catch (IOException e) {
        }
 
@@ -73,7 +45,7 @@ public class LoadImageApp extends Component {
                     System.exit(0);
                 }
             });
-        f.add(new LoadImageApp(image));
+        f.add(new LoadImageApp(image));        
         f.pack();
         f.setVisible(true);
     }
